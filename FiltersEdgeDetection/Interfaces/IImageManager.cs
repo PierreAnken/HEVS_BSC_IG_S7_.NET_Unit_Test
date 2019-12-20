c1e44afac1d35fe5f12e7048d7342fe1037dc930
Pierre Anken <pierre.anken@gmail.com>
2019-12-20 11:27:07 +0100

    fix ImageManager

---
+++ FiltersEdgeDetection/Interfaces/IImageManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+
+namespace FiltersEdgeDetection.Interfaces
+{
+    interface IImageManager
+    {
+        Bitmap load(string path);
+        void save(string path, Bitmap img);
+    }
+}
