0058f716e627b229c38b34fc677936d68372b3be
Pierre Anken <pierre.anken@gmail.com>
2019-12-20 09:38:03 +0100

    Base project structure

---
+++ FiltersEdgeDetection/classes/ExtBitmap.cs
@@ -0,0 +1,220 @@
+﻿/*
+ * The Following Code was developed by Dewald Esterhuizen
+ * View Documentation at: http://softwarebydefault.com
+ * Licensed under Ms-PL 
+*/
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Drawing.Drawing2D;
+using PictureBox.Image.Testes;
+
+namespace ImageEdgeDetection
+{
+    public static class ExtBitmap
+    {
+        public static Bitmap AdaptToSquareCanvas(this Bitmap sourceBitmap, int canvasWidthLenght)
+        {
+            int maxSide = sourceBitmap.Width > sourceBitmap.Height ?
+                          sourceBitmap.Width : sourceBitmap.Height;
+
+            float ratio = maxSide / (float)canvasWidthLenght;
+
+            Bitmap bitmapResult = (sourceBitmap.Width > sourceBitmap.Height ?
+                                    new Bitmap(canvasWidthLenght, (int)(sourceBitmap.Height / ratio))
+                                    : new Bitmap((int)(sourceBitmap.Width / ratio), canvasWidthLenght));
+
+            using (Graphics graphicsResult = Graphics.FromImage(bitmapResult))
+            {
+                graphicsResult.CompositingQuality = CompositingQuality.HighQuality;
+                graphicsResult.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphicsResult.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                graphicsResult.DrawImage(sourceBitmap,
+                                        new Rectangle(0, 0,
+                                            bitmapResult.Width, bitmapResult.Height),
+                                        new Rectangle(0, 0,
+                                            sourceBitmap.Width, sourceBitmap.Height),
+                                            GraphicsUnit.Pixel);
+                graphicsResult.Flush();
+            }
+
+            return bitmapResult;
+        }
+
+        public static Bitmap ConvolutionFilter(this Bitmap sourceBitmap, double[,] xFilterMatrix, double[,] yFilterMatrix = null, int bias = 0, bool grayscale = false, double factor = 1)
+        {
+
+            Boolean oneMatrixMode = yFilterMatrix == null;
+            BitmapData sourceData = sourceBitmap.LockBits(new Rectangle(0, 0,
+                                     sourceBitmap.Width, sourceBitmap.Height),
+                                                       ImageLockMode.ReadOnly,
+                                                  PixelFormat.Format32bppArgb);
+
+            byte[] pixelBuffer = new byte[sourceData.Stride * sourceData.Height];
+            byte[] resultBuffer = new byte[sourceData.Stride * sourceData.Height];
+
+            Marshal.Copy(sourceData.Scan0, pixelBuffer, 0, pixelBuffer.Length);
+            sourceBitmap.UnlockBits(sourceData);
+
+            if (grayscale)
+            {
+                for (int k = 0; k < pixelBuffer.Length; k += 4)
+                {
+                    float rgb = pixelBuffer[k] * 0.11f;
+                    rgb += pixelBuffer[k + 1] * 0.59f;
+                    rgb += pixelBuffer[k + 2] * 0.3f;
+
+                    Color greyscalColor = Color.FromArgb(255, (int)rgb, (int)rgb, (int)rgb);
+                    ImageFilters.SetPixel(pixelBuffer, k, greyscalColor);
+                }
+            }
+
+            int filterOffset = 1;
+            if(oneMatrixMode){
+                int filterWidth = xFilterMatrix.GetLength(1);
+                filterOffset = (filterWidth - 1) / 2;
+            }
+
+
+            for (int offsetY = filterOffset; offsetY <
+                sourceBitmap.Height - filterOffset; offsetY++)
+            {
+                for (int offsetX = filterOffset; offsetX <
+                    sourceBitmap.Width - filterOffset; offsetX++)
+                { 
+                    double greenX = 0, redX = 0, blueX = 0;
+                    double greenY = 0, redY = 0, blueY = 0;
+
+                    int byteOffset = offsetY *sourceData.Stride +offsetX * 4;
+
+                    for (int filterY = -filterOffset;filterY <= filterOffset; filterY++)
+                    {
+                        for (int filterX = -filterOffset;filterX <= filterOffset; filterX++)
+                        {
+                            int calcOffset = byteOffset + (filterX * 4) +  (filterY * sourceData.Stride);
+                            blueX += pixelBuffer[calcOffset] *
+                                      xFilterMatrix[filterY + filterOffset,
+                                              filterX + filterOffset];
+
+                            greenX += pixelBuffer[calcOffset + 1] *
+                                      xFilterMatrix[filterY + filterOffset,
+                                              filterX + filterOffset];
+
+                            redX += pixelBuffer[calcOffset + 2] *
+                                      xFilterMatrix[filterY + filterOffset,
+                                              filterX + filterOffset];
+
+                            if(!oneMatrixMode){
+                                blueY += pixelBuffer[calcOffset] *
+                                          yFilterMatrix[filterY + filterOffset,
+                                                  filterX + filterOffset];
+
+                                greenY += pixelBuffer[calcOffset + 1] *
+                                          yFilterMatrix[filterY + filterOffset,
+                                                  filterX + filterOffset];
+
+                                redY += pixelBuffer[calcOffset + 2] *
+                                          yFilterMatrix[filterY + filterOffset,
+                                                  filterX + filterOffset];
+                            }
+                        }
+                    }
+
+                    double blue, green, red;
+                    if (!oneMatrixMode)
+                    {
+
+                        blue = Math.Sqrt((blueX * blueX) + (blueY * blueY));
+                        green = Math.Sqrt((greenX * greenX) + (greenY * greenY));
+                        red = Math.Sqrt((redX * redX) + (redY * redY));
+
+                    }
+                    else
+                    {
+
+                        blue = factor * blueX + bias;
+                        green = factor * greenX + bias;
+                        red = factor * redX + bias;
+                    }
+
+                    LimitColorRange(ref red);
+                    LimitColorRange(ref green);
+                    LimitColorRange(ref blue);
+
+                    Color newColor = Color.FromArgb(255, (int)red, (int)green, (int)blue);
+                    ImageFilters.SetPixel(resultBuffer, byteOffset, newColor);
+                }
+            }
+
+            Bitmap resultBitmap = new Bitmap(sourceBitmap.Width, sourceBitmap.Height);
+
+            BitmapData resultData = resultBitmap.LockBits(new Rectangle(0, 0,
+                                     resultBitmap.Width, resultBitmap.Height),
+                                                      ImageLockMode.WriteOnly,
+                                                  PixelFormat.Format32bppArgb);
+
+            Marshal.Copy(resultBuffer, 0, resultData.Scan0, resultBuffer.Length);
+            resultBitmap.UnlockBits(resultData);
+
+            return resultBitmap;
+        }
+
+        public static void LimitColorRange(ref double value)
+        {
+            value = (value < 0) ? 0 : (value > 255) ? 255 : value;
+        }
+
+        public static Bitmap LaplacianFilter(this Bitmap sourceBitmap, double[,] matrix, bool grayscale){
+            return ConvolutionFilter(sourceBitmap, matrix, null, 0, grayscale, 1.0);
+        }
+
+        public static Bitmap GaussianFilter(this Bitmap sourceBitmap, double[,] matrix, double factor, bool grayscale)
+        {
+            return ConvolutionFilter(sourceBitmap, matrix, null, 0, grayscale, factor);
+        }
+
+        public static Bitmap Laplacian3x3OfGaussian3x3Filter(this Bitmap sourceBitmap)
+        {
+            Bitmap resultBitmap = GaussianFilter(sourceBitmap, Matrix.Gaussian3x3, 1.0 / 16.0, true);
+            return LaplacianFilter(resultBitmap, Matrix.Laplacian3x3, false);
+        }
+
+        public static Bitmap Laplacian3x3OfGaussian5x5Filter1(this Bitmap sourceBitmap)
+        {
+            Bitmap resultBitmap = GaussianFilter(sourceBitmap, Matrix.Gaussian5x5Type1, 1.0 / 159.0, true);
+            return LaplacianFilter(resultBitmap, Matrix.Laplacian3x3, false);
+        }
+
+        public static Bitmap Laplacian3x3OfGaussian5x5Filter2(this Bitmap sourceBitmap)
+        {
+            Bitmap resultBitmap = GaussianFilter(sourceBitmap, Matrix.Gaussian5x5Type2, 1.0 / 256.0, true);
+            return LaplacianFilter(resultBitmap, Matrix.Laplacian3x3, false);
+        }
+
+        public static Bitmap Laplacian5x5OfGaussian3x3Filter(this Bitmap sourceBitmap)
+        {
+            Bitmap resultBitmap = GaussianFilter(sourceBitmap, Matrix.Gaussian3x3, 1.0 / 16.0, true);
+            return LaplacianFilter(resultBitmap, Matrix.Laplacian5x5, false);
+        }
+
+        public static Bitmap Laplacian5x5OfGaussian5x5Filter1(this Bitmap sourceBitmap)
+        {
+            Bitmap resultBitmap = GaussianFilter(sourceBitmap, Matrix.Gaussian5x5Type1, 1.0 / 159.0, true);
+            return LaplacianFilter(resultBitmap, Matrix.Laplacian5x5, false);
+        }
+
+        public static Bitmap Laplacian5x5OfGaussian5x5Filter2(this Bitmap sourceBitmap)
+        {
+            Bitmap resultBitmap = GaussianFilter(sourceBitmap, Matrix.Gaussian5x5Type2, 1.0 / 256.0, true);
+            return LaplacianFilter(resultBitmap, Matrix.Laplacian5x5, false);
+        }
+
+        public static Bitmap DoubleMatrixFilter(this Bitmap sourceBitmap, double[,] matrix, double[,] matrix2, bool grayscale)
+        {
+            return ConvolutionFilter(sourceBitmap, matrix, matrix2, 0, grayscale);
+        }
+
+    }
+}
---
+++ FiltersEdgeDetection/classes/ImageFilter.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+
+namespace PictureBox.Image.Testes
+{
+    public static class ImageFilters
+    {
+        private static BitmapData bitmapData, bitmapResult;
+
+        public static Bitmap ApplyFilterCrazy(Bitmap bmp)
+        {
+            byte[] pixelBuffer = BitmapToByteArray(bmp, ref bitmapData);
+
+            Color c0,c1,c2;
+
+            for (int x = 0; x < bitmapData.Width; ++x)
+            {
+                for (int y = 0; y < bitmapData.Height; ++y)
+                {
+                    int pixelIndex = y * bitmapData.Stride + x * 4;
+
+                    if(pixelIndex < pixelBuffer.Length) {
+
+                        c0 = GetColorFromBuffer(pixelBuffer,pixelIndex);
+                        c1 = Color.FromArgb(c0.A, c0.G/2, c0.B, c0.R);
+                        c2 = Color.FromArgb(c1.A, c1.G, c1.B, c1.R);
+                        SetPixel(pixelBuffer,pixelIndex, c2);
+
+                    }
+                }
+            }
+
+            Marshal.Copy(pixelBuffer, 0, bitmapData.Scan0, pixelBuffer.Length);
+            bmp.UnlockBits(bitmapData);
+            return bmp;
+        }
+
+        public static Color GetColorFromBuffer(byte[] pixelBuffer, int index)
+        {
+            int blue = pixelBuffer[index];
+            int green = pixelBuffer[index + 1];
+            int red = pixelBuffer[index + 2];
+            int alpha = pixelBuffer[index + 3];
+
+            return Color.FromArgb(alpha, red, green, blue);
+        }
+
+        public static void SetPixel(byte[] pixelBuffer, int index, Color color) {
+            // BGRA
+            pixelBuffer[index] = color.B;
+            pixelBuffer[index + 1] = color.G;
+            pixelBuffer[index + 2] = color.R;
+            pixelBuffer[index + 3] = color.A;
+        }
+
+        public static int GetArea(int x, int y, int limitX, int limitY) {
+
+            int areaX = x / limitX +1;
+            int areaY = y / limitY;
+            int area = areaY * 3 + areaX;
+
+            if (area > 9)
+                throw new ArgumentException("Unable to find area from pixel x/y: " + x + "/" + y);
+
+            return area;
+        }
+
+        public static byte[] BitmapToByteArray(Bitmap image, ref BitmapData bitmapData1) {
+            bitmapData1 = image.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.ReadWrite, image.PixelFormat);
+            byte[] pixelBuffer = new byte[bitmapData1.Stride * bitmapData1.Height];
+            Marshal.Copy(bitmapData1.Scan0, pixelBuffer, 0, pixelBuffer.Length);
+            return pixelBuffer;
+        }
+
+        public static string BitmapToHash(Bitmap image) {
+
+            BitmapData bitmapData = null;
+            byte[] pixelBuffer = BitmapToByteArray(image, ref bitmapData);
+            Marshal.Copy(pixelBuffer, 0, bitmapData.Scan0, pixelBuffer.Length);
+
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            byte[] realHash = md5.ComputeHash(pixelBuffer);
+            string realHashStr = "";
+            foreach (byte hashpart in realHash)
+            {
+                realHashStr += hashpart.ToString();
+            }
+            image.UnlockBits(bitmapData);
+
+            return realHashStr;
+        }
+
+        //apply magic mosaic
+        public static Bitmap ApplyFilterMagicMosaic(Bitmap bmp)
+        {
+
+            int razX = Convert.ToInt32(bmp.Width / 3) + 1;
+            int razY = Convert.ToInt32(bmp.Height / 3) + 1;
+            Bitmap result = new Bitmap(bmp);
+
+            byte[] pixelBufferSource = BitmapToByteArray(bmp, ref bitmapData);
+            byte[] pixelBufferResult = BitmapToByteArray(result, ref bitmapResult);
+
+            int pixelSource,pixelDestination;
+            int workingArea;
+
+            for (int x = 0; x < bmp.Width; ++x)
+            {
+                for (int y = 0; y < bmp.Height; ++y)
+                {
+                    pixelDestination = y * bitmapData.Stride + x * 4;
+
+                    pixelSource = -1;
+                    workingArea = GetArea(x, y, razX, razY);
+
+                    switch (workingArea) {
+                        case 1:
+                        case 3:
+                        case 5:
+                        case 7:
+                            pixelSource = y * bitmapData.Stride + x * 4;
+                            break;
+                        case 2:
+                        case 4:
+                            pixelSource =  x * bitmapData.Stride + y * 4;
+                            break;
+                        case 8:
+                        case 9:
+                            pixelSource = x / 3 * bitmapData.Stride + y / 3 * 4;
+                            break;
+                        case 6:
+                            pixelSource =  x / 2 * bitmapData.Stride + y / 2 * 4;
+                            break;
+                    }
+
+                    if (pixelDestination >= 0 && pixelSource < pixelBufferSource.Length && pixelDestination < pixelBufferResult.Length)
+                    {
+                        Color color = GetColorFromBuffer(pixelBufferSource, pixelSource);
+                        SetPixel(pixelBufferResult, pixelDestination, color);
+                    }
+                }
+            }
+
+            Marshal.Copy(pixelBufferResult, 0, bitmapResult.Scan0, pixelBufferResult.Length);
+            bmp.UnlockBits(bitmapData);
+            result.UnlockBits(bitmapResult);
+            return result;
+        }
+    }
+}
